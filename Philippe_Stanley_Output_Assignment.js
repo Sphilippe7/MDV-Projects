38f6743e5a29a5b41a4c311837f43c73a59bbedb
Sphilippe7 <Solomon924@gmail.com>
2015-03-05 22:37:47 -0500

    The three data types
    created a variable, string, and boolean

--- Philippe_Stanley_Output_Assignment.js
+++ Philippe_Stanley_Output_Assignment.js
@@ -1,3 +1,15 @@
 /**
  * Created by stanleyphilippe on 3/5/15.
  */
+var Sp = StanleyP;
+var birth = 1990;
+var Age = 25;
+Strivetobe = "great at everything I do";
+mission = "my goals in life";
+love = true
+hate = false
+
+
+
+
+
