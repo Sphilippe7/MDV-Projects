886d9d58457b31ed31985798a40bce4b194d3f0f
Sphilippe7 <Solomon924@gmail.com>
2015-03-13 10:27:05 -0400

    start of expressions
    started the coding process for the expressions assignment

---
+++ JS/script.js
@@ -0,0 +1,17 @@
+/**
+ * Created by stanleyphilippe on 3/12/15.
+ */
+// Stanley Philippe
+// 3/12/15
+// SDI Section 2
+// Expressions Assignment
+
+// Tested alert to make sure the js file worked
+
+// Variables
+
+var l = "length";
+var w = "width";
+var p = "perimeter";
+
+
